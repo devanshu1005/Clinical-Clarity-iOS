fa5d889b616bd2cb4002779ab0ccb0b339d0b01f
devanshu1005 <devanshu1005@gmail.com>
2026-07-09 12:10:27 +0530

    Initial Commit

---
+++ Clinical Clarity/Clinical_ClarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clinical_ClarityApp.swift
+//  Clinical Clarity
+//
+//  Created by Rakesh Gupta on 09/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Clinical_ClarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clinical Clarity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clinical Clarity
+//
+//  Created by Rakesh Gupta on 09/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
